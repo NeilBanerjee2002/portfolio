04523b6a7d10487002b9f1b4308a27e240e718cd
NeilBanerjee2002 <neilbanerjee.22.5.2002@gmail.com>
2026-05-07 21:37:03 +0530

    Correct Commmit

--- src/pages/Experience.jsx
+++
@@ -1,8 +0,0 @@
-import ExperienceMain from "../components/Experiance/ExperienceMain";
-
-export default function Experience({ t }) {
-    return (
-        <ExperienceMain t={t} />
-
-    );
-}
--- src/pages/Projects.jsx
+++
@@ -1,7 +0,0 @@
-import ProjectMain from "../components/Project/ProjectMain";
-
-export default function Projects({ t }) {
-    return (
-        <ProjectMain t={t} />
-    );
-}
