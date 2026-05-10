24cdacd8e0559349b01219d36e8bc19b0e6ff0a7
NeilBanerjee2002 <neilbanerjee.22.5.2002@gmail.com>
2026-05-10 19:15:15 +0530

    Changed the SMTP

--- back-end/routes/contactRoutes.js
+++ back-end/routes/contactRoutes.js
@@ -45,11 +45,14 @@ router.post("/", contactLimiter, mandatoryFieldsCheck, async (req, res) => {
     try {
 
         const transporter = nodemailer.createTransport({
-            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
-            }
+            },
+            connectionTimeout: 10000
         });
 
         await transporter.sendMail({
